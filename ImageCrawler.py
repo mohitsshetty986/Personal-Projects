4ecc594db971f10f6ed3c263579d201102cf1f56
Mohit S Shetty <mohitsshetty986@gmail.com>
2020-05-26 14:36:43 +0530

    Update ImageCrawler.py

--- ImageCrawler.py
+++ ImageCrawler.py
@@ -25,36 +25,6 @@ for link in soup.find_all("a",{"class":"lightbox"}):
 	print("loop break")
 
 '''
-*----------------Not working-----------------*
-from bs4 import *
-import requests as rq 
-import os
-
-r2 = rq.get("https://pixabay.com/images/search/sports/")
-soup2 = BeautifulSoup(r2.text,"html.parser")
-
-links = []
-
-var2 = soup2.select('img[src^="https://cdn.pixabay.com/photo/"]')
-
-for img in var2:
-    links.append(img['src'])
-
-for i in links:
-    print(i)
-
-os.mkdir('Images')
-i = 1
-
-for index, img_link in enumerate(links):
-    if i <=10:
-        img_data = rq.get(img_link).content
-        with open("Images\\"+str(index+1)+'.jpg','wb+') as f:
-            f.write(img_data)
-        i=i+1
-    else:
-        f.close()
-        break
 *---------------------for Reddit-------------------*   
 from bs4 import BeautifulSoup
 import requests as rq 
