58e09fdfc2e2db4b5e110a4859a66a075d4b1970
Mohit S Shetty <mohitsshetty986@gmail.com>
2020-05-29 14:12:41 +0530

    Youtube website upload

---
+++ Youtube website/youtubeVideo.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+from pytube import YouTube
+from pytube import Playlist
+import os
+import moviepy.editor as mp
+import time
+
+app=Flask(__name__)
+
+@app.route('/')
+def my_form():
+	return render_template('Home.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+	url = request.form['videourl']
+	ytd=YouTube(url).streams.first().download('E:\\nm\\Youtube website\\static', filename='youtube')
+
+	return render_template('Home.html')
+
+@app.route('/mp3/')
+def mp3_convert():
+    clip=mp.VideoFileClip(r'E:\\nm\\Youtube website\\static\\youtube.mp4')
+    clip.audio.write_audiofile(r'youtube.mp3')
+    time.sleep(10)
+
+    return "Hey mohit video converted to mp3...check in python folder"
+
+if __name__=="__main__":
+	app.run()
