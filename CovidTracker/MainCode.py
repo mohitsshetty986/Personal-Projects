bd9840fdd2b7916f3d7d20a14e61f8e675a541bd
Mohit S Shetty <mohitsshetty986@gmail.com>
2020-06-01 14:30:29 +0530

    Covid Dashboard upload

---
+++ CovidTracker/MainCode.py
@@ -0,0 +1,86 @@
+from __future__ import print_function
+
+import pandas as pd 
+import numpy as np
+import plotly.express as plty
+import plotly.graph_objects as go
+import folium
+import time
+
+from ipywidgets import interact, interactive, fixed, interact_manual
+import ipywidgets as widgets
+
+death_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
+confirmed_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+recovered_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
+country_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv')
+
+death_df.columns=map(str.lower,death_df.columns)
+confirmed_df.columns=map(str.lower,confirmed_df.columns)
+recovered_df.columns=map(str.lower,recovered_df.columns)
+country_df.columns=map(str.lower,country_df.columns)
+
+death_df=death_df.rename(columns={'province/state':'state','country/region':'country'})
+confirmed_df=confirmed_df.rename(columns={'province/state':'state','country/region':'country'})
+recovered_df=recovered_df.rename(columns={'province/state':'state','country/region':'country'})
+country_df=country_df.rename(columns={'country_region':'country'})
+
+sortperworsthit_country_df=country_df.sort_values('confirmed',ascending=False)
+#print(sortperworsthit_country_df)
+
+def highlight_column(x):
+	m='background-color: red'
+	n='background-color: yellow'
+	o='background-color: green'
+	temp_df=pd.DataFrame('',index=x.index,columns=x.columns)
+	temp_df.iloc[:,4]=n
+	temp_df.iloc[:,5]=m
+	temp_df.iloc[:,6]=o
+	return temp_df
+
+sortperworsthit_country_df.style.apply(highlight_column,axis=None)
+
+figure=plty.scatter(sortperworsthit_country_df.head(10), x='country',y='confirmed',size='confirmed',color='country',hover_name='country',size_max=60)
+
+figure.show()
+
+def plot_cases_for_country(country):
+    labels=['confirmed','deaths']
+    colors=['yellow','red']
+    mode_size=[6,8]
+    line_size=[4,5]
+
+    df_list=[confirmed_df,death_df]
+
+    fig=go.Figure()
+
+    for i,df in enumerate(df_list):
+        if country=='World' or country=='world':
+            x_data=np.array(list(df.iloc[:,5:].columns))
+            y_data=np.sum(np.asarray(df.iloc[:,5:]),axis=0)
+        else:
+            x_data=np.array(list(df.iloc[:,5:].columns))
+            y_data=np.sum(np.asarray(df[df['country']==country].iloc[:,5:]),axis=0)
+        fig.add_trace(go.Scatter(x=x_data,y=y_data,mode='lines+markers',name=labels[i],line=dict(color=colors[i],width=line_size[i]),connectgaps=True,text="total "+str(labels[i])+": "+str(y_data[-1])))
+    fig.show()
+#plot_cases_for_country('India')
+
+interact(plot_cases_for_country,country='World');
+
+worldmap=folium.Map(location=[11,0],tiles="cartodbpositron",zoom_start=2,max_zoom=6,min_zoom=2)
+
+for i in range(len(confirmed_df)):
+    folium.Circle(
+    location=[confirmed_df.iloc[i]['lat'], confirmed_df.iloc[i]['long']],
+    fill=True, fill_color='grey',radius=(int(np.log(confirmed_df.iloc[i,-1]+1.00001))+0.2)*50000,
+    color='red', tooltip="<div style='margin: 0; background-color: black; color: white;'>"+
+                    "<h4 style='text-align:center;font-weight: bold'>"+confirmed_df.iloc[i]['country'] + "</h4>"
+                    "<hr style='margin:10px;color: white;'>"+
+                    "<ul style='color: white;;list-style-type:circle;align-item:left;padding-left:20px;padding-right:20px'>"+
+                        "<li>Confirmed: "+str(confirmed_df.iloc[i,-1])+"</li>"+
+                        "<li>Deaths:   "+str(death_df.iloc[i,-1])+"</li>"+
+                        "<li>Death Rate: "+ str(np.round(death_df.iloc[i,-1]/(confirmed_df.iloc[i,-1]+1.00001)*100,2))+ "</li>"+
+                    "</ul></div>",
+    ).add_to(worldmap)
+
+worldmap
---
+++ CovidTracker/Readme.txt
@@ -0,0 +1,22 @@
+Open JupyterLab and execute the code in MAinCode.py file then,
+Save the ipynb file in a folder you find convenient.
+Run in cmd as voila --enable_nbextensions=True, you will find the ipynb file
+in the browser and Done!!!!! ur Dashboard is ready.
+
+Thanks to,
+https://towardsdatascience.com/building-covid-19-analysis-dashboard-using-python-and-voila-ee091f65dcbb
+ 
+Please check it.
+
+Another file named CovidTracking is simple webscrapping from Worldometer website
+that retrieves data in an excel sheet.
+
+
+   .   .
+ {   \   }
+    ---
+	
+sorry for that ^
+               |
+
+(Work by Mohit)  
