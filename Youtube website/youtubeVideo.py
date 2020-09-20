e2e92ebaf198d9946be23d1809b42be8e942c374
Mohit S Shetty <mohitsshetty986@gmail.com>
2020-09-20 12:38:07 +0530

    Update youtubeVideo.py

--- Youtube website/youtubeVideo.py
+++ Youtube website/youtubeVideo.py
@@ -13,16 +13,16 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-	url = request.form['videourl']
-	ytd=YouTube(url).streams.first().download('E:\\nm\\Youtube website\\static', filename='youtube')
+	url = request.form['videourl']		
+	ytd=YouTube(url).streams.first().download('E:\\nm\\Youtube website\\static', filename='youtube') 
 
 	return render_template('Home.html')
 
 @app.route('/mp3/')
-def mp3_convert():
+def mp3_convert():																			
     clip=mp.VideoFileClip(r'E:\\nm\\Youtube website\\static\\youtube.mp4')
-    clip.audio.write_audiofile(r'youtube.mp3')
-    time.sleep(10)
+    clip.audio.write_audiofile(r'youtube.mp3')     
+    time.sleep(10)			
 
     return render_template('Home.html',output="Hey mohit video converted to mp3...check in static folder")
 
