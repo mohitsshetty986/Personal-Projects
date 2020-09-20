3ac6ea7e6971d2ed6eed0b657210ddbb736de9f2
Mohit S Shetty <mohitsshetty986@gmail.com>
2020-09-20 12:55:27 +0530

    Update youtubeVideo.py

--- Youtube website/youtubeVideo.py
+++ Youtube website/youtubeVideo.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template
-from pytube import YouTube
+from flask import Flask, request, render_template                  #main code
+from pytube import YouTube # main crux of this project
 from pytube import Playlist
 import os
 import moviepy.editor as mp
@@ -14,15 +14,15 @@ def my_form():
 @app.route('/', methods=['POST'])
 def my_form_post():
 	url = request.form['videourl']		
-	ytd=YouTube(url).streams.first().download('E:\\nm\\Youtube website\\static', filename='youtube') 
+	ytd=YouTube(url).streams.first().download('E:\\nm\\Personal-Projects\\Youtube website\\static', filename='youtube') #important part of this project
 
 	return render_template('Home.html')
 
 @app.route('/mp3/')
-def mp3_convert():																			
-    clip=mp.VideoFileClip(r'E:\\nm\\Youtube website\\static\\youtube.mp4')
+def mp3_convert():		#additional mp4 to mp3 feature																	
+    clip=mp.VideoFileClip(r'E:\\nm\\Personal-Projects\\Youtube website\\static\\youtube.mp4')
     clip.audio.write_audiofile(r'youtube.mp3')     
-    time.sleep(10)			
+    time.sleep(10)			#wait time to complete download
 
     return render_template('Home.html',output="Hey mohit video converted to mp3...check in static folder")
 
