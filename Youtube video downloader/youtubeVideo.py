137918f7953493cbadbf7a367c9fba861dcf40bd
Mohit S Shetty <mohitsshetty986@gmail.com>
2020-10-30 17:23:27 +0530

    caption added

--- Youtube video downloader/youtubeVideo.py
+++ Youtube video downloader/youtubeVideo.py
@@ -15,6 +15,7 @@ def my_form():
 @app.route('/', methods=['POST'])
 def my_form_post():
 	url = request.form['videourl']		
+	caption(url)
 	# downloading the youtube video using the url
 	ytd=YouTube(url).streams.first().download('static', filename='youtube') 
 
@@ -52,6 +53,15 @@ def img_convert():		# video to image frames
 		else: 
 			break 
 	return render_template('Home.html',output="Hey mohit video converted to image frames...check in static folder")
+
+@app.route('/capt/')
+def caption(url): #video caption capture
+	source = YouTube(url)
+	en_caption = source.captions.get_by_language_code('en')
+	en_caption_convert_to_srt =(en_caption.generate_srt_captions())
+	text_file = open("static\\Caption.txt", "w")
+	text_file.write(en_caption_convert_to_srt)
+	text_file.close()
 
 if __name__=="__main__":
 	app.run()
